234ce434bab5d88019973150244034dfc9fcec46
vinhbt241 <vinhbuith32412000@gmail.com>
2024-09-14 14:28:42 +0700

    init project

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
+  post '/graphql', to: 'graphql#execute'
+  get 'up' => 'rails/health#show', as: :rails_health_check
+  get 'service-worker' => 'rails/pwa#service_worker', as: :pwa_service_worker
+  get 'manifest' => 'rails/pwa#manifest', as: :pwa_manifest
+end
